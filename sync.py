0ba54048e525368d8aba34073270a722598c3c3b
Daniel Fonseca Alves <dalraf@gmail.com>
2018-06-01 13:13:35 -0300

    Adicionado teste de pid

--- sync.py
+++ sync.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import os
+import sys
 from subprocess import Popen, PIPE
-dirdest = "/mnt/dadosufvcredipa01/share"
-dirlocal = "/mnt/dadosufvcredi01/share"
-dirfinal = "ssh://192.168.22.251:5252//" + dirdest
-param1 = "/usr/local/bin/unison  -perms 0 -logfile /var/log/log.unisync -auto -prefer"
-diretorios=[
-    'DOCUMENTOS\ ABERTURA\ DE\ CONTAS/DOCUMENTOS\ DE\ ABERTURA\ DE\ CONTA',
-    'SEGUROS',
-    ]
 
 def execute(cmd):
     print "Executando " + cmd + "\n"
@@ -18,6 +13,27 @@ def execute(cmd):
     if rc > 0:
         print "Erro ao executa " + cmd + "\n o erro foi\n" + output + erro + str(rc)
 
-for dir in diretorios:
-    cmd = param1 + " " + dirlocal + dir + " -batch " + dirlocal + dir + " " + dirfinal + dir
-    execute(cmd)
+pid = str(os.getpid())
+pidfile = "/tmp/mydaemon.pid"
+
+if os.path.isfile(pidfile):
+    print "%s already exists, exiting" % pidfile
+    sys.exit()
+file(pidfile, 'w').write(pid)
+try:
+    dirdest = "/mnt/dadosufvcredipa01/share"
+    dirlocal = "/mnt/dadosufvcredi01/share"
+    dirfinal = "ssh://192.168.22.251:5252//" + dirdest
+    param1 = "/usr/local/bin/unison  -perms 0 -logfile /var/log/log.unisync -auto -prefer"
+    diretorios=[
+        'DOCUMENTOS\ ABERTURA\ DE\ CONTAS/DOCUMENTOS\ DE\ ABERTURA\ DE\ CONTA',
+        'SEGUROS',
+        ]
+
+    for dir in diretorios:
+        cmd = param1 + " " + dirlocal + dir + " -batch " + dirlocal + dir + " " + dirfinal + dir
+        execute(cmd)
+
+finally:
+    os.unlink(pidfile)
+
