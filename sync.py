eb596dc7a70aa6894e0ab2e2c9dea78b22ffc3ca
Daniel Fonseca Alves <dalraf@gmail.com>
2018-06-01 13:33:27 -0300

    Alterado posicionamento, feito auto escape

--- sync.py
+++ sync.py
@@ -5,6 +5,15 @@ import os
 import sys
 from subprocess import Popen, PIPE
 
+dirdest = "/mnt/dadosufvcredipa01/share/"
+dirlocal = "/mnt/dadosufvcredi01/sharenew/"
+dirfinal = "ssh://192.168.22.251:5252//" + dirdest
+param1 = "/usr/local/bin/unison  -perms 0 -logfile /var/log/log.unisync -auto -prefer"
+diretorios=[
+    'DOCUMENTOS ABERTURA DE CONTAS',
+    'SEGUROS',
+    ]
+
 def execute(cmd):
     print "Executando " + cmd + "\n"
     saida = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
@@ -17,21 +26,14 @@ pid = str(os.getpid())
 pidfile = "/tmp/mydaemon.pid"
 
 if os.path.isfile(pidfile):
-    print "%s already exists, exiting" % pidfile
+    print "%s arquivo existe, saindo..." % pidfile
     sys.exit()
 file(pidfile, 'w').write(pid)
 try:
-    dirdest = "/mnt/dadosufvcredipa01/share/"
-    dirlocal = "/mnt/dadosufvcredi01/sharenew/"
-    dirfinal = "ssh://192.168.22.251:5252//" + dirdest
-    param1 = "/usr/local/bin/unison  -perms 0 -logfile /var/log/log.unisync -auto -prefer"
-    diretorios=[
-        'DOCUMENTOS\\ ABERTURA\\ DE\\ CONTAS',
-        'SEGUROS',
-        ]
 
     for dir in diretorios:
-        cmd = param1 + " " + dirlocal + dir + " -batch " + dirlocal + dir + " " + dirfinal + dir
+        direxe = dir.replace(" ","\\ ")
+        cmd = param1 + " " + dirlocal + direxe + " -batch " + dirlocal + direxe + " " + dirfinal + direxe
         execute(cmd)
 
 finally:
