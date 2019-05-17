0979a0ae307d7685f46440eaa597eaeb6e2013a7
Daniel Fonseca Alves <dalraf@gmail.com>
2019-05-17 10:14:00 -0300

    Adicionado arquivo de parametros

---
+++ param.py
@@ -0,0 +1,12 @@
+diretorios=[
+    'GERENCIA_CONTROLE_FINANCEIRO/Gerência_de_Controles\ Financeiros/CARTÃO\ DE\ ASSINATURA\ -\ COOPERADOS',
+    'CONTROLE_INTERNO/PRODUCAO\ ATENDIMENTO',
+    'GERENCIA_CONTROLE_FINANCEIRO/CAPITAL\ SOCIAL\ -\ RESGATES\ EVENTUAIS/Registros\ Resgates',
+    'GERENCIA_RELACIONAMENTO_NEGOCIO/Gerencia_de_\ Contas/Produto\ e\ Serviços/Sicoob\ consórcio/',
+    'GERENCIA_RELACIONAMENTO_NEGOCIO/ATENDIMENTO/SILVANE',
+    'GERENCIA_RELACIONAMENTO_NEGOCIO/Controle\ de\ Agenda\ \ e\ banco\ de\ horas\ do\ setor/Banco\ de\ Horas/', 
+    'scanner',
+    ]
+dirdest = "/mnt/dadoscoopemg01/share/"
+dirlocal = "/mnt/dadoscoopemgsede/share/"
+dirfinal = "ssh://192.168.102.252:5252//" + dirdest
--- sync.py
+++ sync.py
@@ -4,26 +4,12 @@
 import os
 import sys
 from subprocess import run, PIPE
-
-dirdest = "/mnt/dadoscoopemg01/share/"
-dirlocal = "/mnt/dadoscoopemgsede/share/"
-dirfinal = "ssh://192.168.102.252:5252//" + dirdest
+import param
 
 logfile = open("/var/log/log.unisync", "a")
 
 param1 = "/usr/local/bin/unison  -perms 0 -auto -prefer"
 unison_env = os.environ.copy()
-
-
-diretorios=[
-    'GERENCIA_CONTROLE_FINANCEIRO/Gerência_de_Controles\ Financeiros/CARTÃO\ DE\ ASSINATURA\ -\ COOPERADOS',
-    'CONTROLE_INTERNO/PRODUCAO\ ATENDIMENTO',
-    'GERENCIA_CONTROLE_FINANCEIRO/CAPITAL\ SOCIAL\ -\ RESGATES\ EVENTUAIS/Registros\ Resgates',
-    'GERENCIA_RELACIONAMENTO_NEGOCIO/Gerencia_de_\ Contas/Produto\ e\ Serviços/Sicoob\ consórcio/',
-    'GERENCIA_RELACIONAMENTO_NEGOCIO/ATENDIMENTO/SILVANE',
-    'GERENCIA_RELACIONAMENTO_NEGOCIO/Controle\ de\ Agenda\ \ e\ banco\ de\ horas\ do\ setor/Banco\ de\ Horas/', 
-    'scanner',
-    ]
 
 def execute(cmd):
     print ("Executando " + cmd + "\n")
